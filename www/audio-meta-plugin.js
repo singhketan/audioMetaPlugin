a6439681b4175a9657ec8e4db3b20bbce174d009
Ketan Singh <singhketan@users.noreply.github.com>
2015-04-02 23:29:30 +0530

    Update audio-meta-plugin.js

--- www/audio-meta-plugin.js
+++ www/audio-meta-plugin.js
@@ -1,7 +1,6 @@
 /*global cordova, module*/
 module.exports = {
     getID3: function (filePath, successCallback, errorCallback) {
-        console.log("In here!");
 	cordova.exec(successCallback, errorCallback, "audioMetaPlugin", "pullID3", [filePath]);
     }
 };
