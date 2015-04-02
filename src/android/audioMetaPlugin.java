e167f22a2be8b4e693b4500b5eb820bf1e7d01c1
Ketan Singh <singhketan@users.noreply.github.com>
2015-04-02 23:43:28 +0530

    Update audioMetaPlugin.java

--- src/android/audioMetaPlugin.java
+++ src/android/audioMetaPlugin.java
@@ -3,33 +3,69 @@ package com.ketan.plugin.audio;
 import org.apache.cordova.*;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.media.MediaMetadataRetriever;
 
 public class audioMetaPlugin extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
-        if (action.equals("pullID3")) {
-            
-			MediaMetadataRetriever metaRetriver = new MediaMetadataRetriever();
-			String filePath = args.getString(0);
-			metaRetriver.setDataSource(filePath);
+	public static String ALBUM;
+	public static String ARTIST;
+	public static String GENRE;
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals("pullID3")) { 
+			JSONObject argObject = args.getJSONObject(0);
+			JSONObject r = new JSONObject();
 			
-			try{
-				String result = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).toString(); 
-			}
-			catch(e){
-				String result = "Unknown";
-			}
+		   // Here goes our custom code
+			//String path = "/sdcard";
+			String getPath = argObject.getString("FilePath");
 
-			callbackContext.success(result);
+			MediaMetadataRetriever metaRetriver = new MediaMetadataRetriever(); 
+			metaRetriver.setDataSource(fullpath);
+			
+			try 
+			{
+				ALBUM = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM).toString();
+				ARTIST = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).toString(); 
+				GENRE = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE).toString();
+			}
+			catch (Exception e)
+			{
+				ALBUM = "unknown-album";
+				ARTIST = "unknown-artist";
+				GENRE = "unknown-genre";
+			}
+			
+			if(ALBUM.equals("") || ALBUM == null)
+			{
+				ALBUM = "unknown-album";
+			}
+			
+			if(ARTIST.equals("") || ARTIST == null)
+			{
+				ARTIST = "unknown-artist";
+			}
+			
+			if(GENRE.equals("") || GENRE == null)
+			{
+				GENRE = "unknown-genre";
+			}
+			
+			r.put("songname", songName);
+			r.put("fullpath", getPath.toString());
+			r.put("album", ALBUM.toString());
+			r.put("artist", ARTIST.toString());
+			r.put("genre", GENRE.toString());
+			
+			callbackContext.success(r.toString());
 			return true;
-
-        } else {
-	    callbackContext.error("Unknown method.");
-            return false;
-
-        }
-    }
+		}
+		else
+		{
+			callbackContext.error("No such action defined");
+			return false;
+		}
+	}
 }
