7db5b0ee09b36857838746cb8fa9f5d3e4519ae2
Ketan Singh <singhketan@users.noreply.github.com>
2015-04-02 23:19:42 +0530

    Update audio-meta-plugin.js

--- www/audio-meta-plugin.js
+++ www/audio-meta-plugin.js
@@ -1,19 +1,5 @@
 /*global cordova, module*/
-var exec = require('cordova/exec'),
-channel = require('cordova/channel');
-
-
-// Override back button action to prevent being killed
-document.addEventListener('backbutton', function () {}, false);
-
-// Called before 'deviceready' listener will be called
-channel.onCordovaReady.subscribe(function () {
-    // Device plugin is ready now
-    channel.onCordovaInfoReady.subscribe(function () {
-        // Set the defaults
-        console.log("At least here now");
-        exports.getID3 = function(filePath, successCallback, errorCallback) {
+ window.getID3 = function(filePath, successCallback, errorCallback) {
+ 			console.log("IN here!");
 			cordova.exec(successCallback, errorCallback, "audioMetaPlugin", "pullID3", [filePath]);
 		};
-    });
-});
