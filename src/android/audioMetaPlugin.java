30041e1dd6a01289835a08e07bc052426fc11204
Ketan Singh <singhketan@users.noreply.github.com>
2015-04-03 17:16:43 +0530

    Added threading.

--- src/android/audioMetaPlugin.java
+++ src/android/audioMetaPlugin.java
@@ -15,45 +15,46 @@ public class audioMetaPlugin extends CordovaPlugin {
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("pullID3")) { 
-			JSONObject r = new JSONObject();
-			MediaMetadataRetriever metaRetriver = new MediaMetadataRetriever(); 
-			String filePath = args.getString(0);
-			metaRetriver.setDataSource(filePath);
+			cordova.getThreadPool().execute(new Runnable() {
+		                public void run() {
+		                    JSONObject r = new JSONObject();
+					MediaMetadataRetriever metaRetriver = new MediaMetadataRetriever(); 
+					String filePath = args.getString(0);
+					metaRetriver.setDataSource(filePath);
+					
+					try {
+						ALBUM = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM).toString();
+	
+					}
+					catch (Exception e){
+						ALBUM = "unknown-album";
+					}
+					
+					try {
+						ARTIST = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).toString(); 
+					}
+					catch (Exception e){
+						ARTIST = "unknown-artist";
+					}
+					
+					try {
+						GENRE = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE).toString();
+					}
+					catch (Exception e){
+						GENRE = "unknown-genre";
+					}
+					
 			
-			try 
-			{
-				ALBUM = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ALBUM).toString();
-				ARTIST = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST).toString(); 
-				GENRE = metaRetriver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_GENRE).toString();
-			}
-			catch (Exception e)
-			{
-				ALBUM = "unknown-album";
-				ARTIST = "unknown-artist";
-				GENRE = "unknown-genre";
-			}
+				
+					r.put("album", ALBUM.toString());
+					r.put("artist", ARTIST.toString());
+					r.put("genre", GENRE.toString());
+					
+					callbackContext.success(r.toString());
+					return true;
+		                }
+        		 });
 			
-			if(ALBUM.equals("") || ALBUM == null)
-			{
-				ALBUM = "unknown-album";
-			}
-			
-			if(ARTIST.equals("") || ARTIST == null)
-			{
-				ARTIST = "unknown-artist";
-			}
-			
-			if(GENRE.equals("") || GENRE == null)
-			{
-				GENRE = "unknown-genre";
-			}
-		
-			r.put("album", ALBUM.toString());
-			r.put("artist", ARTIST.toString());
-			r.put("genre", GENRE.toString());
-			
-			callbackContext.success(r.toString());
-			return true;
 		}
 		else
 		{
